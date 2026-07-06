f05a5942470a32b38b5f789113d534d229d0588c
Ludmila <ludamocuk29@gmail.com>
2026-07-06 21:34:20 +0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -13,19 +13,19 @@
 // }
 
 // export default App;
-//setCount //Змінює значення count.//Каже React: "Перемалюй компонент."
-// import { useState } from "react";
-// function App() {
-//   const [count, setCount] = useState(0);
-//   return (
-//     <>
-//       <h1>{count}</h1>
-//       <button onClick={() => setCount(count + 1)}>+</button>
-//       <button onClick={() => setCount((prevCount) => prevCount + 1)}>+</button>
-//       <button onClick={() => setCount(0)}>Скинути</button>
-//     </>
-//   );
-// }
-// export default App;
+// setCount //Змінює значення count.//Каже React: "Перемалюй компонент."
+import { useState } from "react";
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>+</button>
+      <button onClick={() => setCount(0)}>Скинути</button>
+    </>
+  );
+}
+export default App;
 
 /////////////////////////////////////Todo List.\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
