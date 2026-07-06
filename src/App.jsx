06a0dd97d26137a6a69fc3d7f0d524acedf6ee65
Ludmila <ludamocuk29@gmail.com>
2026-07-06 21:27:27 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+// import { useState } from "react";
+
+// function App() {
+//   const [count, setCount] = useState(0);
+
+//   return (
+//     <>
+//       <h1>{count}</h1>
+
+//       <button onClick={() => setCount(count + 1)}>+</button>
+//     </>
+//   );
+// }
+
+// export default App;
+//setCount //Змінює значення count.//Каже React: "Перемалюй компонент."
+// import { useState } from "react";
+// function App() {
+//   const [count, setCount] = useState(0);
+//   return (
+//     <>
+//       <h1>{count}</h1>
+//       <button onClick={() => setCount(count + 1)}>+</button>
+//       <button onClick={() => setCount((prevCount) => prevCount + 1)}>+</button>
+//       <button onClick={() => setCount(0)}>Скинути</button>
+//     </>
+//   );
+// }
+// export default App;
+
+/////////////////////////////////////Todo List.\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
